6b4fd37defcd1345bb7ef25c8495849f59598c0b
farizmursyidan <farizmursyidan@gmail.com>
2020-06-14 21:26:21 +0700

    added react reveal fading effect

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from 'elements/Button'
 import BrandIcon from 'parts/IconText'
+import Fade from 'react-reveal/Fade'
 
 export default function Header(props) {
 
@@ -9,28 +10,30 @@ export default function Header(props) {
   }
 
   return (
-    <header className="spacing-sm">
-      <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container">
-          <BrandIcon />
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ml-auto">
-              <li className={`nav-item${getNavLinkClass("/")}`}>
-                <Button className="nav-link" type="link" href="/">Home</Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                <Button className="nav-link" type="link" href="/browse-by">Browse By</Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                <Button className="nav-link" type="link" href="/stories">Stories</Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                <Button className="nav-link" type="link" href="/agents">Agents</Button>
-              </li>
-            </ul>
+    <Fade>
+      <header className="spacing-sm">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="container">
+            <BrandIcon />
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav ml-auto">
+                <li className={`nav-item${getNavLinkClass("/")}`}>
+                  <Button className="nav-link" type="link" href="/">Home</Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                  <Button className="nav-link" type="link" href="/browse-by">Browse By</Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                  <Button className="nav-link" type="link" href="/stories">Stories</Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                  <Button className="nav-link" type="link" href="/agents">Agents</Button>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </Fade>
   )
 }
--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -1,38 +1,43 @@
 import React from 'react';
 import Button from 'elements/Button';
+import Fade from 'react-reveal/Fade';
 
 export default function MostPicked(props) {
   return (
     <section ref={props.refMostPicked}>
-      <div className="container">
-        <h4 className="mb-3">Most Picked</h4>
-        <div className="container-grid">
-          {
-            props.data.map((item, index) => {
-              return (
-                <div key={`mostpicked-${index}`} className={`item column-4${index === 0 ? " row-2" : " row-1"}`}>
-                  <div className="card card-featured">
-                    <div className="tag">
-                      ${item.price} <span className="font-weight-light">per {item.unit}</span>
-                    </div>
-                    <figure className="img-wrapper">
-                      <img src={item.imageUrl} alt={item.name} className="img-cover" />
-                    </figure>
-                    <div className="meta-wrapper">
-                      <Button type="link" className="stretched-link d-block text-white product-name" href={`/properties/${item._id}`}>
-                        <h5>{item.name}</h5>
-                      </Button>
-                      <span style={{ fontWeight: 300 }}>
-                        {item.city}, {item.country}
-                      </span>
-                    </div>
+      <Fade bottom>
+        <div className="container">
+          <h4 className="mb-3">Most Picked</h4>
+          <div className="container-grid">
+            {
+              props.data.map((item, index) => {
+                return (
+                  <div key={`mostpicked-${index}`} className={`item column-4${index === 0 ? " row-2" : " row-1"}`}>
+                    <Fade bottom delay={300 * index}>
+                      <div className="card card-featured">
+                        <div className="tag">
+                          ${item.price} <span className="font-weight-light">per {item.unit}</span>
+                        </div>
+                        <figure className="img-wrapper">
+                          <img src={item.imageUrl} alt={item.name} className="img-cover" />
+                        </figure>
+                        <div className="meta-wrapper">
+                          <Button type="link" className="stretched-link d-block text-white product-name" href={`/properties/${item._id}`}>
+                            <h5>{item.name}</h5>
+                          </Button>
+                          <span style={{ fontWeight: 300 }}>
+                            {item.city}, {item.country}
+                          </span>
+                        </div>
+                      </div>
+                    </Fade>
                   </div>
-                </div>
-              )
-            })
-          }
+                )
+              })
+            }
+          </div>
         </div>
-      </div>
-    </section>
+      </Fade >
+    </section >
   )
 }
